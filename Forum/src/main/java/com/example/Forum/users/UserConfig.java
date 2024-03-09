2b3c6964621a1990f3267581bf73526a4d8499a7
jaleks3 <s24592@pjwstk.edu.pl>
2024-03-09 14:07:31 +0100

    Refactored the seeding mechanism

--- Forum/src/main/java/com/example/Forum/users/UserConfig.java
+++ Forum/src/main/java/com/example/Forum/users/UserConfig.java
@@ -1,27 +1,13 @@
 package com.example.Forum.users;
 
+import com.example.Forum.threads.ForumThread;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Configuration
 public class UserConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(UserRepository repository){
-        return args -> {
-            User user1 = new User(
-                    "john_doe",
-                    "john.doe@example.com",
-                    "password123"
-            );
-            User user2 = new User(
-                    "jane_smith",
-                    "jane.smith@example.com",
-                    "securepassword"
-            );
-            repository.saveAll(List.of(user1,user2));
-        };
-    }
 }
