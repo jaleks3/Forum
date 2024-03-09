e216d0ec7e0caef4907a6e0a91cfcc67ae59b731
jaleks3 <s24592@pjwstk.edu.pl>
2024-03-09 14:11:53 +0100

    Optimized imports

--- Forum/src/main/java/com/example/Forum/threads/ThreadConfig.java
+++ Forum/src/main/java/com/example/Forum/threads/ThreadConfig.java
@@ -1,10 +1,6 @@
 package com.example.Forum.threads;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Configuration
 public class ThreadConfig {
--- Forum/src/main/java/com/example/Forum/users/UserConfig.java
+++ Forum/src/main/java/com/example/Forum/users/UserConfig.java
@@ -1,12 +1,6 @@
 package com.example.Forum.users;
 
-import com.example.Forum.threads.ForumThread;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Configuration
 public class UserConfig {
