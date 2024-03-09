a6c4979001ce62a530da5495b3a8ec95e719b403
jaleks3 <s24592@pjwstk.edu.pl>
2024-03-09 14:08:51 +0100

    Refactored seeding mechanism

---
+++ Forum/src/main/java/com/example/Forum/threads/ThreadConfig.java
@@ -0,0 +1,11 @@
+package com.example.Forum.threads;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Configuration
+public class ThreadConfig {
+}
