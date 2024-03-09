328dc77341f84310db7ff266d12ff14c6cbad625
jaleks3 <s24592@pjwstk.edu.pl>
2024-03-09 10:36:48 +0100

    Implemented getUser in UserController.java

--- Forum/src/main/java/com/example/Forum/users/UserController.java
+++ Forum/src/main/java/com/example/Forum/users/UserController.java
@@ -2,6 +2,7 @@ package com.example.Forum.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class UserController {
     @GetMapping
     public List<User> getUsers(){
         return userService.getUsers();
+    }
+
+    @GetMapping(path = "{userId}")
+    User getUser(@PathVariable Long userId){
+        return userService.GetUserById(userId);
     }
 
 }
